5e013cfcec91545db949383bf855fb26d28b51f8
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2026-03-04 10:19:19 +0700

    membuat java Kelas User sebagai entitas JPA yang memetakan tabel users dengan kolom id, name, dan age, menggunakan Lombok untuk otomatis membuat getter, setter, constructor, dan builder.

---
+++ src/main/java/com/deploy/praktikum1/modul/entity/User.java
@@ -0,0 +1,28 @@
+package com.deploy.praktikum1.modul.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private int age;
+}
